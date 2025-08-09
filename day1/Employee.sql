8c3f8fb143ed1143989647dae7ed25cc6474dd08
Tamojit Das <tamojitdas181007@gmail.com>
2025-08-09 17:38:45 +0530

    Day 2 Assignment: 50%

--- day1/Employee.sql
+++ day1/Employee.sql
@@ -31,4 +31,16 @@ select emp_name || ', ' || dept_id "Employee and Title" from emp_151;
 
 select emp_id, emp_name, salary * 12 as Annual_Salary from emp_151;
 
+
+/* Day 2 */
+select emp_id, emp_name from emp_151 where emp_name like '%a%' and emp_name like '%e%';
+
+select emp_name, dept_id from emp_151 where commission is null;
+
+select emp_name as Employee, salary as Monthly_Salary from emp_151 where salary between 25000 and 42000 and (dept_id = 109 or dept_id = 118);
+
+select emp_id, emp_name from emp_151 where emp_name like 'J%' or emp_name like 'A%' or emp_name like 'M%' order by emp_name;
+
+select emp_id, emp_name, dept_id, salary + nvl(commission, 0) as Total_Salary from emp_151;
+
 commit;
